4f9e1cf07979df57051df18fbad301a0abea114a
fumikaobayashi <fumika.obayashi@gmail.com>
2019-06-25 12:21:01 +0900

    Update index

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,14 +1,18 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update, :destroy]
+
   def index
-
-  end
-
-  def new
-
+    @blogs = Blog.all.order("created_at DESC").page(params[:page]).per(20)
+    @blog = Blog.new
   end
 
   def create
-
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   def show
@@ -16,14 +20,27 @@ class BlogsController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-
+    if @blog.update(blog_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    
+    @blog.destroy if @blog.user.id == current_user.id
+    redirect_to root_path
+  end
+
+  private
+  def set_blog
+    @blog = Blog.find(params[:id])    
+  end
+
+  def blog_params
+    params.require(:blog).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
