b4352b68ed99884dee4cc176f2197575d84850ce
fumikaobayashi <fumika.obayashi@gmail.com>
2019-06-25 12:20:41 +0900

    carrierwave

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,7 @@ class Blog < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+  mount_uploader :image, ImageUploader
+
+  validates :content, presence: true, unless: :image? 
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_fit: [200, 200]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -44,4 +45,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
+
 end
+
