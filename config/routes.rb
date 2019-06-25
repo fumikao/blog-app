fe4e0e6650b30784f4213dcc7e9b7178e4f1cc7e
fumikaobayashi <fumika.obayashi@gmail.com>
2019-06-25 12:19:26 +0900

    Updat routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'blogs#index'
-  resources :blogs do
+  resources :blogs, exceprt: :new do
     resources :comments, except: [:index, :new, :show]
     resources :likes, only: [:create, :destroy]
   end
