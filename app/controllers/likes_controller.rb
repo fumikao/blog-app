25e71698a7f4c5a967937dcef302f83cd4bd7515
fumikaobayashi <fumika.obayashi@gmail.com>
2019-06-25 15:29:48 +0900

    Update user_like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,12 @@
 class LikesController < ApplicationController
   def create
+    Like.create(blog_id: params[:blog_id], user_id: current_user.id)
+    redirect_to root_path
   end
 
   def destroy
-    
+    like = Like.find_by(blog_id: params[:blog_id], user_id: current_user.id)
+    like.destroy
+    redirect_to root_path
   end
 end
